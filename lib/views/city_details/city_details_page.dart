0b6e466fb7d9befa4d288df062c1c91f164173be
Yunus Can iP <yunuscan0196@gmail.com>
2022-03-21 15:54:54 +0300

    City Details appbar navigation update

--- lib/views/city_details/city_details_page.dart
+++ lib/views/city_details/city_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasketiya/main.dart';
 
 import 'city_details_body.dart';
 
@@ -8,7 +9,16 @@ class CityDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            appRouter.pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+          ),
+        ),
+      ),
       body: const SafeArea(
         child: CityDetailsBody(),
       ),
